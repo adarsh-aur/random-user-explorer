3a350840839e74841b6a89dcb07eef5e033280ce
Adarsh Ranjan <adarsh.aur@gmail.com>
2026-05-27 00:50:04 +0530

    Inital setup for the random user explorer using async await fetch api

---
+++ index.js
@@ -0,0 +1,72 @@
+const fetchBtn = document.getElementById("fetch-btn");
+const userCard = document.getElementById("user-card");
+const loadingText = document.getElementById("loading");
+const errorText = document.getElementById("error");
+
+const API_URL = "https://randomuser.me/api/?results=5";
+
+function showSkeletons() {
+    userCard.innerHTML = "";
+
+    for (let i = 0; i < 6; i++) {
+        userCard.innerHTML += `
+        <div class="skeleton">
+            <div class="circle"></div>
+            <div class="line long"></div>
+            <div class="line short"></div>
+        </div>
+        `;
+    }
+}
+
+fetchBtn.addEventListener("click", async () => {
+    userCard.innerHTML = "";
+    errorText.innerHTML = "";
+    loadingText.innerHTML = "Loading..";
+    
+    showSkeletons();
+
+    try {
+        const response = await fetch(API_URL);
+
+        if (!response.ok) throw new Error('Something went wrong');
+
+        const data = await response.json();
+
+        const user = data.results[0];
+
+        display(user);
+
+    } catch (error) {
+        errorText.textContent = error.message;
+    } finally {
+        loadingText.textContent = "";
+    }
+})
+
+function display(user) {
+    userCard.innerHTML = `
+    <div class="card">
+      <img src="${user.picture.large}" alt="User Image">
+
+      <h2>
+        ${user.name.first} ${user.name.last}
+      </h2>
+
+      <p>
+        ${user.email}
+      </p>
+
+      <p>
+        ${user.location.country}
+      </p>
+    </div>
+    `;
+} 
+
+
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
